fa7d70de2432e211d8dd2e58e5597816ea05f0fd
Veno veno <naufalpriatna26@gmail.com>
2024-06-14 15:50:36 +0700

    add: String(Escaping & Literal)

--- Basic/script.js
+++ Basic/script.js
@@ -1,5 +1,6 @@
 console.log("Javascript from external file");
 
+// Variable (Let, Var, Const)
 //Variable with let
 let name = "Venomare";
 //Tampilkan data
@@ -17,7 +18,7 @@ console.log(firstName);
 //Scope from let
 let lastName = "Mare";
 {
-  let lastName = "Mares"; //hanya bisa di jalanankan didalam dictionary
+  let lastName = "Mares"; //hanya bisa di jalankan didalam dictionary
   console.log(lastName);
 }
 console.log(lastName);
@@ -25,7 +26,7 @@ console.log(lastName);
 //Scope from var
 var middleName = "Nice";
 {
-  var middleName = "Noble"; //global bisa di diluar dari dictionary
+  var middleName = "Noble"; //global bisa di jalankan diluar dari dictionary
   console.log(middleName);
 }
 console.log(middleName);
@@ -43,7 +44,7 @@ console.log(birthDay);
 //birthDay = "25 Mei 1995";
 //end Variable
 
-//Data Types
+//Data Types (String, number, boolean, undifined)
 let dataString = "Veno"; //string
 console.log(dataString)
 console.log(typeof dataString);
@@ -66,3 +67,23 @@ console.log(typeof dataKosong);
 dataKosong = "Veno";
 console.log(dataKosong);
 console.log(typeof dataKosong);
+//end Data Types
+
+//String (Escaping & Literal)
+//escaping string (\' \" \\ \n \r \t \b \f)
+let data1 = 'Veno said "Hello world!"';
+console.log(data1);
+let data2 = "Venom said \"Lets go\"";
+console.log(data2);
+let data3 = "Veno is on holiday \nLets go";
+console.log(data3);
+
+//Literal string (template literal string)
+let fName = "Veno";
+let lName = "Mare";
+let age = 18;
+let fullName = age + " " + fName + " " + lName; //not like this
+console.log(fullName);
+//yes like this
+let biodata = `${fName} ${lName} dengan umur ${age}`;
+console.log(biodata);
